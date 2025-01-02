fcf442885fb25f59c60a39806bbc3f1a74a0f5d5
Keijiro Takahashi <keijiro@gmail.com>
2025-01-02 23:38:09 +0900

    Shared preferences

---
+++ Assets/Editor/EditorToolsPreference.cs
@@ -0,0 +1,83 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Klak.EditorTools {
+
+// Preference accessor
+public static class Preferences
+{
+    public static bool FpsCapperEnable
+      { get => EditorPrefs.GetBool(Keys.FpsCapperEnable, false);
+        set => EditorPrefs.SetBool(Keys.FpsCapperEnable, value); }
+
+    public static int FpsCapperFrameRate
+      { get => EditorPrefs.GetInt(Keys.FpsCapperFrameRate, 60);
+        set => EditorPrefs.SetInt(Keys.FpsCapperFrameRate, value); }
+
+    public static float VfxGraphZoomStep
+      { get => EditorPrefs.GetFloat(Keys.VfxGraphZoomStep, 1);
+        set => EditorPrefs.SetFloat(Keys.VfxGraphZoomStep, value); }
+
+    // Preference keys
+    static class Keys
+    {
+        public const string FpsCapperEnable
+          = "Klak.EditorTools.FpsCapper.Enable";
+
+        public const string FpsCapperFrameRate
+          = "Klak.EditorTools.FpsCapper.FrameRate";
+
+        public const string VfxGraphZoomStep
+          = "Klak.EditorTools.VfxGraph.ZoomStep";
+    }
+}
+
+// Preference provider (GUI)
+public sealed class PreferencesProvider : SettingsProvider
+{
+    public PreferencesProvider()
+      : base("Preferences/Editor Tools", SettingsScope.User) {}
+
+    public override void OnGUI(string search)
+    {
+        var h = EditorGUIUtility.singleLineHeight / 2;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(h);
+        GUILayout.BeginVertical();
+        GUILayout.Space(h);
+
+        EditorGUI.BeginChangeCheck();
+
+        var fc_enable = Preferences.FpsCapperEnable;
+        var fc_frameRate = Preferences.FpsCapperFrameRate;
+        var vg_zoomStep = Preferences.VfxGraphZoomStep;
+
+        GUILayout.Label("FPS Capper", EditorStyles.boldLabel);
+        fc_enable = EditorGUILayout.Toggle("Enable", fc_enable);
+        fc_frameRate = EditorGUILayout.IntField("Target Frame Rate", fc_frameRate);
+
+        GUILayout.Space(h);
+
+        GUILayout.Label("VFX Graph", EditorStyles.boldLabel);
+        vg_zoomStep = EditorGUILayout.FloatField("Zoom Step Scale", vg_zoomStep);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Preferences.FpsCapperEnable = fc_enable;
+            Preferences.FpsCapperFrameRate = fc_frameRate;
+            Preferences.VfxGraphZoomStep = vg_zoomStep;
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+
+        base.OnGUI(search);
+    }
+
+    [SettingsProvider]
+    public static SettingsProvider GetSettingsProvider()
+      => new PreferencesProvider();
+}
+
+} // namespace Klak.EditorTools 
--- Assets/Editor/VfxGraphZoomStepModifier.cs
+++ Assets/Editor/VfxGraphZoomStepModifier.cs
@@ -1,22 +1,14 @@
 using System;
 using System.Reflection;
-using System.Collections.ObjectModel;
 using UnityEditor;
-using UnityEngine;
 
 namespace Klak.EditorTools {
 
 [InitializeOnLoad]
-public static class VfxGraphExtraPreference
+public static class VfxGraphZoomStepModifier
 {
-    static VfxGraphExtraPreference()
+    static VfxGraphZoomStepModifier()
       => EditorWindow.windowFocusChanged += OnWindowFocusChanged;
-
-    public const string ZoomStepScaleKey = "VFX.Extra.ZoomStepScale";
-
-    public static float ZoomStepScale
-      { get => EditorPrefs.GetFloat(ZoomStepScaleKey, 1);
-        set => EditorPrefs.SetFloat(ZoomStepScaleKey, value); }
 
     public static void OnWindowFocusChanged()
     {
@@ -42,32 +34,10 @@ public static class VfxGraphExtraPreference
           ("SetupZoom",
            BindingFlags.Public | BindingFlags.Instance, null, argTypes, null);
 
-        var args = new object[] { 0.125f, 8, 0.15f * ZoomStepScale, 1};
+        var args = new object[]
+          { 0.125f, 8, 0.15f * Preferences.VfxGraphZoomStep, 1 };
         setupZoomMethod.Invoke(graphView, args);
     }
-}
-
-class VfxGraphExtraPreferenceProvider : SettingsProvider
-{
-    public VfxGraphExtraPreferenceProvider()
-      : base("Preferences/Visual Effects Extra", SettingsScope.User) {}
-
-    public override void OnGUI(string search)
-    {
-        EditorGUI.BeginChangeCheck();
-
-        var scale = VfxGraphExtraPreference.ZoomStepScale;
-        scale = EditorGUILayout.FloatField("Zoom Step Scale", scale);
-
-        if (EditorGUI.EndChangeCheck())
-            VfxGraphExtraPreference.ZoomStepScale = scale;
-
-        base.OnGUI(search);
-    }
-
-    [SettingsProvider]
-    public static SettingsProvider PreferenceSettingsProvider()
-      => new VfxGraphExtraPreferenceProvider();
 }
 
 } // namespace Klak.EditorTools 
